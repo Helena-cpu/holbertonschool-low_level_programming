dab577c9d0b8c19c44d25b083b0e311c26428b2e
helena <helenagutierres1@gmail.com>
2021-02-05 14:10:30 +0000

    c program that shows the last digit of a variable

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - Entry point
  *
@@ -12,14 +13,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-	printf("%d this is number positive\n", n);
+	printf("%d is positive\n", n);
 	else if (n == 0)
-	{
-		printf("%d this is a number zero\n", n);
-	}
+		printf("%d is zero\n", n);
 	else
-	{
-		printf("%d this is a number negative\n", n);
-	}
+	printf("%d is negative\n", n);
 	return (0);
 }

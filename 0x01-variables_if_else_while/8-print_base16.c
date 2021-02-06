4e610cf98de3c40a57f14366838447552f0991b5
helena <helenagutierres1@gmail.com>
2021-02-06 04:03:08 +0000

    returns to base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point
+*
+* Return Always 0 (Sucess)
+*/
+
+int main(void)
+{
+	int a;
+	char b;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+	}
+	for (b = 'a'; b <= 'f'; b++)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+	return (0);
+}

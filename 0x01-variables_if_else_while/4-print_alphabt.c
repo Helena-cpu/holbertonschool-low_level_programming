23d821d55483faa25b538ff93b3979913cffb939
helena <helenagutierres1@gmail.com>
2021-02-06 01:37:12 +0000

    print the alphabet except x characters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a != 'q' && a != 'e')
+			putchar (a);
+	}
+	putchar ('\n');
+	return (0);
+}
